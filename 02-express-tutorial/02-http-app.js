735abde8146392dd95899a0bbed7c3dfbcc436d0
Shy L <63942346+ShyLee873@users.noreply.github.com>
2022-03-24 17:51:53 -0400

    week 4 answers

---
+++ 02-express-tutorial/02-http-app.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const {readFileSync} = require('fs');
+
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
+
+const server = http.createServer((req, res) => {
+    // console.log(req.method)
+    const url = req.url;
+    if(url === '/'){
+    res.writeHead(200, {'content-type':'text/html'})
+    res.write(homePage)
+    res.end()
+    // About
+    } else if(url === '/about'){
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h1>About Page</h1>')
+        res.end()
+    // Styles    
+    } else if(url === '/styles.css'){
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(homeStyles)
+        res.end()
+    // Logo    
+    } else if(url === '/logo.svg'){
+        res.writeHead(200, {'content-type':'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    // Logic    
+    } else if(url === '/browser-app.js'){
+        res.writeHead(200, {'content-type':'text/javascript'})
+        res.write(homeLogic)
+        res.end()
+    //404    
+    }  else {
+        res.writeHead(404, {'content-type':'text/html'})
+        res.write('<h1>Not Found</h1>')
+        res.end()
+    }
+})
+
+server.listen(5000)
--- 02-express-tutorial/05-express-all-static.js
+++ 02-express-tutorial/05-express-all-static.js
@@ -4,10 +4,6 @@ const app = express()
 
 app.use(express.static('./navbar-app'))
 
-// app.get('/', (req, res) => {
-//     res.status(200).sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-// })
-
 app.all('*', (req, res) => {
     res.status(404).send('Resource not found')
 })
