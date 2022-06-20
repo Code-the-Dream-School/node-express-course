c8c2142a5eee0fcc1ee605f0b3d51f62d9ad9c44
dahlak76 <dahlakkeleta@gmail.com>
2022-06-19 20:21:17 -0500

    week3 complete

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,16 @@
-console.log('Express Tutorial')
+// const express = require('express')
+// const app = express()
+// const path = require('path')
+
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, './new-public/index.html'))
+// })
+
+// app.get('/sample', (req, res) => {
+//   res.send('this is working')
+// })
+
+// app.listen(3000, () => {
+//   console.log('server is listening on port 3000...')
+// })
