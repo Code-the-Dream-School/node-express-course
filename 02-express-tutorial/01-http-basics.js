5bde11abd5e6bf91cf4eacd02c8a50e805ea9cd7
aiman <abduldanmaihaja@gmail.com>
2023-01-24 18:45:36 +0100

    answers for lesson 3

--- 02-express-tutorial/01-http-basics.js
+++ 02-express-tutorial/01-http-basics.js
@@ -1,15 +1,15 @@
 const http = require("http");
-const { readFileSync } = require("fs");
-const data = readFileSync("./new-public/index.html");
+
 const server = http.createServer((req, res) => {
   const url = req.url;
+
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write(data);
+    res.write("<h1>home page</h1>");
     res.end();
-  } else if (url === "/sample") {
+  } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>This is working</h1>");
+    res.write("<h1>about page</h1>");
     res.end();
   } else {
     res.writeHead(404, { "content-type": "text/html" });
@@ -18,4 +18,4 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000);
+server.listen(5000);
