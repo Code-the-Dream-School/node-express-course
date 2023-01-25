2339cab8f74a3840c2b3f71ad90e2db815f5647e
MJP2421 <garciapatriciomelanie89@gmail.com>
2023-01-25 00:41:59 -0500

    I did lesson 2 update

---
+++ 01-node-tutorial/answers/practice1.js
@@ -0,0 +1,5 @@
+const os = require('os');
+const sentence = require('./practice2');
+
+console.log(os.platform(), os.release());
+module.exports = sentence;
---
+++ 01-node-tutorial/answers/practice2.js
@@ -0,0 +1,2 @@
+const sentence = "Hello, my name is Melanie and I am 18 years old "
+module.exports = sentence;
