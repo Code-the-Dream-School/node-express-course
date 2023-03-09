4875c6d841011371b799655f1fce088a30284b96
Tooba <jtooba70@gmail.com>
2023-03-09 18:09:28 +0500

    answers for lesson 1

---
+++ 01-node-tutorial/answers/03-modules.js
@@ -0,0 +1,8 @@
+const { john, peter } = require('./names.js')
+const sayHi = require('./utils.js')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
