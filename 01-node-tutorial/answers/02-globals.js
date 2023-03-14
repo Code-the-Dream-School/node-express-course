2d13d4ead60fa310ef0e3aa977f8495adf9fe67b
Catherine Njenga <freshiah.catherine@gmail.com>
2023-03-14 12:45:23 +0300

    answers for lesson 1

---
+++ 01-node-tutorial/answers/02-globals.js
@@ -0,0 +1,13 @@
+// A few examples of globals in Node.js
+// __dirname
+// __filename
+// require
+// module
+// process
+
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("Hello world");
+}, 1000);
+console.log(__filename);
