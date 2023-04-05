36489e8be05bc1dd79480f1873b62597b581b774
LongJourney47 <fernandezjw07@gmail.com>
2023-04-05 14:21:18 -0400

    week2 additonal assginment pt2

--- 01-node-tutorial/answers/practice3.js
+++ 01-node-tutorial/answers/practice3.js
@@ -1,10 +1,16 @@
-fs = require('fs').promises;
+fs = require("fs").promises;
 
-writeFile(
-    "01-node-tutorial/answers/content/practice2.txt",
-    content,
-  
-    (err) => {
-      console.log(err);
+const makeFile = async () => {
+  try {
+    await fs.writeFile("01-node-tutorial/answers/content/practice2.txt", "This is the first line.\n");
+    for (let i = 1; i <= 10; i++) {
+      await fs.writeFile("01-node-tutorial/answers/content/practice2.txt", `This is line ${i}.\n`, {
+        flag: "a",
+      });
     }
-  );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+makeFile();
