604b2aaa85aa55799fe8f55798d4d23abb0ae5fd
David Villavicencio <111402717+dsvill06@users.noreply.github.com>
2023-07-11 21:44:01 -0400

    answers for week6

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,18 +3,24 @@ const app = express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require("dotenv").config()
+const notFound = require("./middleware/not-found.js")
+const errorHandlerMiddleware = require("./middleware/error-handler")
+
 
 //middleware
+app.use(express.static("./public"))
 app.use(express.json())
 
 //Routes
-app.get("/",(req,res) => {
-    res.send("Task Manager App")
-})
+
 
 app.use("/api/v1/tasks",tasks)
 
-const port = 3000;
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try{
