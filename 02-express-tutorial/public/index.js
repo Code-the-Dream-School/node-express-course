2c23bff608d705b0e81778b42786502006b262ab
M'BAYI YANN BANGAH(BINO) <81714858+Bino26@users.noreply.github.com>
2023-07-16 21:32:29 +0000

    Update index.js

--- 02-express-tutorial/public/index.js
+++ 02-express-tutorial/public/index.js
@@ -2,6 +2,7 @@ document.getElementById("get-products").addEventListener("click", async () => {
   const response = await fetch("api/v1/products");
   const products = await response.json();
   const productsDiv = document.getElementById("products-list");
+  
   productsDiv.innerHTML = "";
   products.forEach((product) => {
     const productDiv = document.createElement("div");
