63154a02bdcada27f3593ff43264b82984625223
AndzhurT <andjur38@gmail.com>
2023-07-23 18:39:53 -0400

    Task Manager API - part2

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 // schema of how the task will look like
 const TaskSchema = new mongoose.Schema({
-  name:String, completed:Boolean
+  name: {
+    type: String, 
+    require: [true, 'You must provide a name'], 
+    trim: true, 
+    maxlength: [20, 'Name can not have more than 20 characters'],   
+  }, 
+  completed: {
+    type: Boolean, 
+    default: false, 
+  },
 }) 
 
 module.exports = mongoose.model('Task', TaskSchema)
