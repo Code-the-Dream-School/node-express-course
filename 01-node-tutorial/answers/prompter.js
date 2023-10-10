7f8cad0a7975e4a9bc0c2482f8bc1d2af9478542
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-10 10:55:01 +0300

    edit prompter.js to train event emitters

--- 01-node-tutorial/answers/prompter.js
+++ 01-node-tutorial/answers/prompter.js
@@ -100,5 +100,10 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000);
-console.log("The server is listening on port 3000.");
+server.on('request', (req) => {
+  console.log("event received: ", req.method, req.url);
+});
+
+server.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
