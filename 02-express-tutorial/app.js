926219aab962f49c21f84120e4a5e4b948e477a4
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-24 20:44:44 +0300

    add logger middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,72 +1,21 @@
 const express = require('express');
 const app = express();
 
-const { products } = require("./data");
+const logger = (req, res, next) => {
+    const method = req.method;
+    const url = req.url;
+    console.log(method, url);
+    next();
+}
 
-app.use(express.static('./public'));
+app.use(["/", "/about"], logger);
 
-app.get('/api/v1/test', (req, res) => {
-    res.json({ message: "It worked!" });
-});
-
-app.get('/api/v1/products', (req, res) => {
-    const newProducts = products.map(product => {
-        const { id, name, image } = product;
-        return { id, name, image }
-    })
-    res.json(newProducts);
-});
-
-app.get('/api/v1/products/:productID', (req, res) => {
-    const idToFind = parseInt(req.params.productID); 
-    const singleProduct = products.find(product => product.id === idToFind);
-
-    if(!singleProduct) {
-        return res.status(404).send({ message: 'That product was not found.' });
-    };
-
-    return res.json(singleProduct);
-});
-
-app.get('/api/v1/query', (req, res) => {
-    const { search, limit, regex, maxprice } = req.query;
-    let sortedProducts = [...products];
-
-    if (regex) {
-        try {
-            const searchTermRegex = new RegExp(regex, 'i'); 
-            sortedProducts = sortedProducts.filter(product => searchTermRegex.test(product.name));
-        } catch {
-            return res.status(400).json({ success: false, message: 'Invalid regex pattern' });
-        }
-    } else if (search) {
-        sortedProducts = sortedProducts.filter(product => {
-            return product.name.toLowerCase().startsWith(search.toLowerCase());
-        });
-    }
-
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-
-    if(sortedProducts.length < 1) {
-        return res.status(200).json({ success: true, data: [] });
-    }
-
-    if (maxprice) {
-        const validPrice = parseFloat(maxprice);
-        if (validPrice) {
-            sortedProducts = sortedProducts.filter(product => product.price < validPrice);
-        } else {
-            return res.status(400).json({ success: false, message: 'Invalid maxprice value' });
-        }
-    } 
-    res.status(200).json(sortedProducts);
-});
-
-app.all('*', (req, res) => {
-    res.status(404).send('<h1>Page not found</h1>');
-});
+app.get('/', (req, res) => {
+    res.send('Home');
+})
+app.get('/about', (req, res) => {
+    res.send('About');
+})
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
