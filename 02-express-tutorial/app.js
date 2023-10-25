cfe6c0efceae62615fde89040634e8883ddc30a7
Anna Pestova <106414157+AnnaPestova1@users.noreply.github.com>
2023-10-25 14:38:46 -0400

    commented unnecessary logic

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -46,10 +46,10 @@ app.get("/api/v1/query", (req, res)=> {
         return product.price<priceLessThan
         })
     }
-    if(sortedItems.length<1){
-        // res.status(200).send("no products match your search")
-       return  res.status(200).json({success: true, data: []})
-    }
+    // if(sortedItems.length<1){
+    //     // res.status(200).send("no products match your search")
+    //    return  res.status(200).json({success: true, msg: "no items founded"})
+    // }
     return  res.status(200).json({success: true, data: sortedItems})
 //   return  res.status(200).json(sortedItems)
     // res.send("hello world")
