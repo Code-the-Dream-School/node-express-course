e14c46fa1dbe9e854396e4e10067d285268046c0
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-25 23:12:49 +0300

    implement authentication via cookies

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
 
-const peopleRouter = require('./routes/people')
+const peopleRouter = require('./routes/people');
+const authRouter = require('./routes/auth');
 
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded( {extended: false} ));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/v1/people', peopleRouter);
+app.use('/', authRouter);
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
