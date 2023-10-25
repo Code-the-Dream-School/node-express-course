0bf92541194b7d6787ef91241a2a4089db33234c
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-25 23:32:36 +0300

    refactor routing structure for person-related endpoints

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -9,10 +9,12 @@ const {
     deletePerson
 } = require('../controllers/people');
 
-router.get('/', getPeople);
-router.get('/:id', getPerson);
-router.post('/', addPerson);
-router.put('/:id', updatePerson);
-router.delete('/:id', deletePerson);
+router.route('/')
+    .get(getPeople)
+    .post(addPerson);
+router.route('/:id')
+    .get(getPerson)
+    .put(updatePerson)
+    .delete(deletePerson);
 
 module.exports = router;
