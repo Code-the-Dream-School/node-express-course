4116e95ca042ff01f50f5dd4b0f11b3b5e248358
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-25 21:18:40 +0300

    refactor-add controller

--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -1,21 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { people } = require('../data');
+const {
+    getPeople,
+    addPerson,
+} = require('../controllers/people');
 
-router.get('/', (req, res) => {
-    res.status(200).json( {success: true, data: people} );
-});
-
-router.post('/', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res
-            .status(400)
-            .json({ success: false, message: "Please provide a name" });
-    }
-    people.push({ id: people.length + 1, name: name });
-    res.status(201).json({ success: true, data: [...people] });
-});
+router.get('/', getPeople);
+router.post('/', addPerson);
 
 module.exports = router;
