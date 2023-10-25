38f7cca93ce8e8ed15207b0efa33289256342981
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-25 21:26:33 +0300

    add PUT and DELETE endpoints for 'people' entity

--- 02-express-tutorial/controllers/people.js
+++ 02-express-tutorial/controllers/people.js
@@ -15,7 +15,39 @@ const addPerson = (req, res) => {
     res.status(201).json({ success: true, data: [...people] });
 };
 
+const updatePerson = (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const person = people.find(person=> person.id === Number(id));
+    if (!person) {
+        return res  
+            .status(400)
+            .json({ success: false, message: `No person is found with id ${id}` });
+    }
+    const newPeople = people.map(person=> {
+        if(person.id === Number(id)){
+            person.name = name;
+        }
+        return person;
+    })
+    res.status(200).json( {success: true, data: newPeople} );
+};
+
+const deletePerson = (req, res) => {
+    const { id } = req.params;
+    const person = people.find(person=> person.id === Number(id));
+    if (!person) {
+        return res
+            .status(400)
+            .json({ success: false, message: `No person is found with id ${id}` });
+    }
+    const newPeople = people.filter(person => person.id !== Number(id));
+    return res.status(200).json( {success: true, data: newPeople} );
+};
+
 module.exports = {
     getPeople,
     addPerson,
+    updatePerson,
+    deletePerson
 };
--- 02-express-tutorial/routes/people.js
+++ 02-express-tutorial/routes/people.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const {
     getPeople,
     addPerson,
+    updatePerson,
+    deletePerson
 } = require('../controllers/people');
 
 router.get('/', getPeople);
 router.post('/', addPerson);
+router.put('/:id', updatePerson);
+router.delete('/:id', deletePerson);
 
 module.exports = router;
