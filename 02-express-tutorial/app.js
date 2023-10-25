a6fef6950401fc8b328eeb9262364182622c2123
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-25 21:03:41 +0300

    refactor-add router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,24 +1,13 @@
 const express = require('express');
 const app = express();
 
-const { people } = require('./data');
+const peopleRouter = require('./routes/people')
 
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded( {extended: false} ));
 app.use(express.json());
 
-app.get('/api/v1/people', (req, res) => {
-    res.status(200).json( {success: true, data: people} );
-});
-
-app.post('/api/v1/people', (req, res) => {
-    const { name } = req.body;
-    if (!name) {
-        return res.status(400).json({ success: false, message: "Please provide a name" });
-    }
-    people.push({ id: people.length + 1, name: name });
-    res.status(201).json( {success: true, person: name} );
-});
+app.use('/api/v1/people', peopleRouter);
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
