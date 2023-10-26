cbb340c92807d585efd16a55f8694fdd8952453f
AnnaSolovykh <ms.teachy.solo@gmail.com>
2023-10-26 09:51:54 +0300

    refactor middleware sequence for enhanced readability in app file

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const app = express();
 const cookieParser = require('cookie-parser');
 
-const peopleRouter = require('./routes/people');
-const authRouter = require('./routes/auth');
+const app = express();
 
 app.use(express.static('./methods-public'));
 app.use(express.urlencoded( {extended: false} ));
 app.use(express.json());
 app.use(cookieParser());
+
+const peopleRouter = require('./routes/people');
+const authRouter = require('./routes/auth');
 
 app.use('/api/v1/people', peopleRouter);
 app.use('/', authRouter);
