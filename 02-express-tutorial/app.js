d90a5a6a227bf302e494032315e7f3451a0da698
Anna <114008959+AnnaSolovykh@users.noreply.github.com>
2023-11-03 13:31:11 +0300

    Revert "Node - Week4 Assignment"

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,68 @@
 const express = require('express');
-const cookieParser = require('cookie-parser');
-
 const app = express();
 
-app.use('/api/v1/people', express.static('./methods-public')); 
-app.use('/api/v1', express.static('./public'));
-app.use(express.urlencoded( {extended: false} ));
-app.use(express.json());
-app.use(cookieParser());
+const { products } = require("./data");
 
-const peopleRouter = require('./routes/people');
-const authRouter = require('./routes/auth');
-const productsRouter = require('./routes/products'); 
+app.use(express.static('./public'));
 
-app.use('/api/v1/people', peopleRouter);
-app.use('/', authRouter);
-app.use('/api/v1', productsRouter); 
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: "It worked!" });
+});
+
+app.get('/api/v1/products', (req, res) => {
+    const newProducts = products.map(product => {
+        const { id, name, image } = product;
+        return { id, name, image }
+    })
+    res.json(newProducts);
+});
+
+app.get('/api/v1/products/:productID', (req, res) => {
+    const idToFind = parseInt(req.params.productID); 
+    const singleProduct = products.find(product => product.id === idToFind);
+
+    if(!singleProduct) {
+        return res.status(404).send({ message: 'That product was not found.' });
+    };
+
+    return res.json(singleProduct);
+});
+
+app.get('/api/v1/query', (req, res) => {
+    const { search, limit, regex, maxprice } = req.query;
+    let sortedProducts = [...products];
+
+    if (regex) {
+        try {
+            const searchTermRegex = new RegExp(regex, 'i'); 
+            sortedProducts = sortedProducts.filter(product => searchTermRegex.test(product.name));
+        } catch {
+            return res.status(400).json({ success: false, message: 'Invalid regex pattern' });
+        }
+    } else if (search) {
+        sortedProducts = sortedProducts.filter(product => {
+            return product.name.toLowerCase().startsWith(search.toLowerCase());
+        });
+    }
+
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+
+    if(sortedProducts.length < 1) {
+        return res.status(200).json({ success: true, data: [] });
+    }
+
+    if (maxprice) {
+        const validPrice = parseFloat(maxprice);
+        if (validPrice) {
+            sortedProducts = sortedProducts.filter(product => product.price < validPrice);
+        } else {
+            return res.status(400).json({ success: false, message: 'Invalid maxprice value' });
+        }
+    } 
+    res.status(200).json(sortedProducts);
+});
 
 app.all('*', (req, res) => {
     res.status(404).send('<h1>Page not found</h1>');
@@ -24,6 +71,3 @@ app.all('*', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
-
-
-
