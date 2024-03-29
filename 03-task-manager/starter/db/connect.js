65ae38bf865aaea7d944b168066d8d99435db700
SonaliBedge <bedgesonali@yahoo.com>
2024-03-29 11:20:00 -0700

    Task Manager-Part-1

--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 const connectionString = "mongodb+srv://NodeExpressDB:Pwdpsrv123@nodeexpressprojects.yqaxcjt.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority&appName=NodeExpressProjects"
-                            // mongodb+srv://NodeExpressDB:<password>@nodeexpressprojects.yqaxcjt.mongodb.net/?retryWrites=true&w=majority&appName=NodeExpressProjects
-
 
 const connectDB =(url) => {
     return mongoose.connect(url, {
@@ -11,16 +9,3 @@ const connectDB =(url) => {
         useUnifiedTopology: true,
 })}
 module.exports = connectDB
-
-
-// mongoose.connect(connectionString)
-// .then(() => console.log('CONNECTED TO THE DB...'))
-// .catch((err) => console.log(err));
-// mongoose.connect(connectionString, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-// })
-// .then(() => console.log('CONNECTED TO THE DB...'))
-// .catch((err) => console.log(err));
