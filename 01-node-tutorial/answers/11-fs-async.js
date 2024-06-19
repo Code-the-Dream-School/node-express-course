fc5b4808b0b9bcb389cd2695dc60d92bbfcc9c32
olgakahraman <ol.khrmn@gmail.com>
2024-06-18 21:56:12 -0400

    Update 11-fs-async.js

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -1,13 +1,16 @@
-const { writeFile } = require('fs');
-console.log("at start")
+const { writeFile } = require("fs");
+console.log("at start");
 
-writeFile('./temporary/output.txt', 'here is a text from 11-fs-async.js file', (err, result) => {
+writeFile(
+  "./temporary/output.txt",
+  "here is a text from 11-fs-async.js file",
+  (err, result) => {
     console.log("at point 1");
-    if(err) {
-        console.log("this error happened: ", err);
-
-    }else {
-    console.log("Appending flag", { flag: "a" });
+    if (err) {
+      console.log("this error happened: ", err);
+    } else {
+      console.log("Appending flag", { flag: "a" });
     }
-})
-console.log("at end")
+  }
+);
+console.log("at end");
