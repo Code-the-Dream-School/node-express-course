9139f2c269934befa7cc85da024fb4f61826a6e0
olgakahraman <ol.khrmn@gmail.com>
2024-06-18 21:11:13 -0400

    created 11-fs-async.js file witj asynchronous function writeFile which created output.txt file in temporary folder

---
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -0,0 +1,13 @@
+const { writeFile } = require('fs');
+console.log("at start")
+
+writeFile('./temporary/output.txt', 'here is a text from 11-fs-async.js file', (err, result) => {
+    console.log("at point 1");
+    if(err) {
+        console.log("this error happened: ", err);
+
+    }else {
+    console.log("Appending flag", { flag: "a" });
+    }
+})
+console.log("at end")
