27a6e7734b32cfa4deb06046174e60def8071571
olgakahraman <ol.khrmn@gmail.com>
2024-06-24 10:11:50 -0400

    Update 11-fs-async.js

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -1,16 +1,39 @@
 const { writeFile } = require("fs");
 console.log("at start");
 
-writeFile(
-  "./temporary/output.txt",
-  "here is a text from 11-fs-async.js file",
-  (err, result) => {
+writeFile("./temporary/fileB.txt", "This is line 1\n", (err) => {
+  if (err) {
+    console.error("This error happened: ", err);
+  } else {
     console.log("at point 1");
-    if (err) {
-      console.log("this error happened: ", err);
-    } else {
-      console.log("Appending flag", { flag: "a" });
-    }
+
+    writeFile(
+      "./temporary/fileB.txt",
+      "This is line 2, appended.\n",
+      { flag: "a" },
+      (err) => {
+        if (err) {
+          console.error("This error happened: ", err);
+        } else {
+          console.log("at point 2");
+
+          writeFile(
+            "./temporary/fileB.txt",
+            "This is line 3, also appended.\n",
+            { flag: "a" },
+            (err) => {
+              if (err) {
+                console.error("This error happened: ", err);
+              } else {
+                console.log("at point 3");
+                console.log("File write completed.");
+              }
+            }
+          );
+        }
+      }
+    );
   }
-);
+});
+
 console.log("at end");
