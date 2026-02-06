91f1731e6ca1ab43e868ac5e6a1134e840919e13
SteppesLover <amanzhanovalikhan@gmail.com>
2026-02-06 10:13:38 -0800

    Minor bugfixes

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,4 +1,3 @@
-const product = require('../models/product')
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
--- 04-store-api/starter/models/product.js
+++ 04-store-api/starter/models/product.js
@@ -15,9 +15,9 @@ const productSchema = new mongoose.Schema({
     }, 
     rating:{
         type: Number, 
-        default: 4.5,
+          default: 4.5,
     },
-    createAt:{
+    createdAt:{
         type: Date,
         default: Date.now()
     },
